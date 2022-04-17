2fc8ef5f2a753869025dfab146ff49241f1051f7
Wu Justin <justin.cathay@gmail.com>
2022-04-17 23:51:32 +0800

    修改readme

--- js/http.js
+++ js/http.js
@@ -1,6 +1,6 @@
 /** 使用heroku api */
-//const http = 'https://lit-headland-53855.herokuapp.com/ArticleList'
+const http = 'https://lit-headland-53855.herokuapp.com/ArticleList'
 
 /** 使用Node.js + MongoDB 在本地端運行 */
-const http = 'http://127.0.0.1:3005/ArticleList'
+//const http = 'http://127.0.0.1:3005/ArticleList'
 
